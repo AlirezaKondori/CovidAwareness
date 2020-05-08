91c48d4717d0db77457a4e0d68042143c440f101
AlirezaKondori <alirezako82@gmail.com>
2020-05-07 23:15:18 -0700

    parsed data added

--- Medicaldevice/app/src/main/java/com/example/medicaldevice/MainActivity.java
+++ Medicaldevice/app/src/main/java/com/example/medicaldevice/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.medicaldevice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.Text);
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new doit().execute();
+            }
+        });
+    }
+    public class doit extends AsyncTask<Void,Void,Void>{
+
+        String cases;
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+                Document doc = Jsoup.connect("https://www.worldometers.info/coronavirus/").get();
+                cases = doc.text();
+
+            }catch(Exception e) {e.printStackTrace();}
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            text.setText(cases);
+
+        }
     }
 }
